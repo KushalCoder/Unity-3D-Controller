7a7abe60b991f3439f76c8662712d39228afffd9
KushalCoder <115026944+KushalCoder@users.noreply.github.com>
2022-10-04 21:49:40 +0530

    Add files via upload

---
+++ 3D Character Controller/PlayerBindings.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBindings : MonoBehaviour
+{
+    public float speed = 30f;
+    float xVel;
+    float zVel;
+    public CharacterController cont;
+    public float gravity = 13f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDist = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    public float jumpVelocity = 5f;
+
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundDist,groundMask);
+
+        if (isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButton("Jump") && isGrounded){
+            velocity.y = jumpVelocity;
+        }
+
+        xVel = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        zVel = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        Vector3 move = transform.right * xVel + transform.forward * zVel;
+        cont.Move(move * speed * Time.deltaTime);
+        velocity.y -= gravity * Time.deltaTime;
+        cont.Move(velocity * Time.deltaTime);
+    }
+}
